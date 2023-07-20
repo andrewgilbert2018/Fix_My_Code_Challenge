9f88d84b8fc534072d800b54619754e741afc000
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-07-19 19:49:40 -0700

    user.py

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
+""" 
+User class
+"""
 
-class square():
+class User():
+    """ Documentation """
+
+    def __init__(self):
+        """ Documentation """
+        self.__email = None
+
+    @email.setter
+    def email(self, value):
+        """ Documentation """
+        if type(value) is not str:
+            raise TypeError("email must be a string")
+        self.__email = value
+
+    @property
+    def email(self):
+        """ Documentation """
+        return self.__email
+   
     
-    width = 0
-    height = 0
-
-    
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-    def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
-
-    def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
-
-    def __str__(self):
-        return "{}/{}".format(self.width, self.height)
-
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
-    print(s)
-    print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    u = User()
+    u.email = "john@snow.com"
+    print(u.email)
